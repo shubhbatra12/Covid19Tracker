e2d620bed88dbcab68ce073151e48a3fca36f76d
shubh <shubhbatra12@gmail.com>
2020-05-04 16:14:53 +0530

    Added India Detail

---
+++ app/src/main/java/com/shubh/covid19tracker/India/CoronaIndService.kt
@@ -0,0 +1,11 @@
+package com.shubh.covid19tracker.India
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface CoronaIndService {
+
+    @GET("states")
+    suspend fun getMyState(): Response<SearchResponse>
+
+}
---
+++ app/src/main/java/com/shubh/covid19tracker/India/StateAdapter.kt
@@ -0,0 +1,33 @@
+package com.shubh.covid19tracker.India
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.shubh.covid19tracker.R
+import kotlinx.android.synthetic.main.item_state.view.*
+
+class StateAdapter(val data: List<State>) : RecyclerView.Adapter<StateAdapter.UserViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_state, parent, false)
+        )
+    }
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) = holder.bind(data[position])
+
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: State) = with(itemView) {
+            nameView.text = item.name
+            totalCases.text = "Total : "+item.total.toString()
+            deadCases.text = "Deaths : "+item.death.toString()
+            recoveredCases.text = "Recovered : "+item.cured.toString()
+        }
+    }
+
+}

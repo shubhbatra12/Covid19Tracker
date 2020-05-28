4d661ac4774b575d40b558f3219c6bf1057c87ca
shubh <shubhbatra12@gmail.com>
2020-05-28 13:33:42 +0530

    Added Splash Screen

--- app/src/main/java/com/shubh/covid19tracker/MainActivity.kt
+++ app/src/main/java/com/shubh/covid19tracker/MainActivity.kt
@@ -7,11 +7,24 @@ import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.view.*
 import android.widget.SearchView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import com.shubh.covid19tracker.India.IndiaActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.activity_detail.Tests
+import kotlinx.android.synthetic.main.activity_detail.TestsPM
+import kotlinx.android.synthetic.main.activity_detail.actCases
+import kotlinx.android.synthetic.main.activity_detail.contName
+import kotlinx.android.synthetic.main.activity_detail.criticalCases
+import kotlinx.android.synthetic.main.activity_detail.deadCases
+import kotlinx.android.synthetic.main.activity_detail.imgFlag
+import kotlinx.android.synthetic.main.activity_detail.lastUpdt
+import kotlinx.android.synthetic.main.activity_detail.recoveredCases
+import kotlinx.android.synthetic.main.activity_detail.totCases
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.internet_dialog.view.*
 import kotlinx.android.synthetic.main.item_country.view.*
@@ -19,6 +32,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
 
 const val DARK_THEME = "DarkTheme"
 const val RC_SETTINGS = 111
@@ -30,6 +44,10 @@ class MainActivity : AppCompatActivity() {
     val list = arrayListOf<Country>()
     val originalList = arrayListOf<Country>()
     val adapter = CountryAdapter(list)
+
+    private val mAppUnitId: String by lazy {
+        "ca-app-pub-5689524874061492~2817334170"
+    }
 
     private val sharedPreferences by lazy {
         PreferenceManager.getDefaultSharedPreferences(this)
@@ -64,6 +82,22 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
+        initializeBannerAd(mAppUnitId)
+        loadBannerAd()
+
+    }
+
+    private fun initializeBannerAd(appUnitId: String) {
+
+        MobileAds.initialize(this, appUnitId)
+
+    }
+
+    private fun loadBannerAd() {
+
+        val adRequest = AdRequest.Builder()
+            .build()
+        adView.loadAd(adRequest)
     }
 
     private fun changeTheme() {
@@ -194,14 +228,68 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun openDialogDetail(CountryName: CharSequence) {
+
+        val mDialogViewD = LayoutInflater.from(this).inflate(R.layout.dialog_detail, null, false)
+
+
+        val mBuilderD = AlertDialog.Builder(this)
+            //.setView(mDialogViewD)
+            .setCancelable(true)
+        val mAlertDialogD = mBuilderD.show()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val thisCountry = withContext(Dispatchers.IO) { Client.api.getUserDet(CountryName as String) }
+//            Picasso.get().load(thisCountry.countryInfo?.flag).into(imgFlag)
+//            contNameD.text = thisCountry.country
+//            totCasesD.text = "Total Cases : " + convertToIndianStandard(thisCountry.cases.toString())
+//            actCasesD.text = "Active Cases : " + convertToIndianStandard(thisCountry.active.toString()) + " ↑" + convertToIndianStandard(thisCountry.todayCases.toString())
+//            recoveredCasesD.text = "Recovered Cases : " + convertToIndianStandard(thisCountry.recovered.toString())
+//            deadCasesD.text =
+//                "Deceased : " + convertToIndianStandard(thisCountry.deaths.toString()) + " ↑" + thisCountry.todayDeaths.toString()
+//            criticalCasesD.text = "Critical Cases : " + thisCountry.critical
+//            TestsD.text = "Total Test : " + convertToIndianStandard(thisCountry.tests.toString())
+//            TestsPMD.text = "Tests Per Million : " + convertToIndianStandard(thisCountry.testsPerOneMillion.toString())
+//
+//            val myFormat = "h:mm a, d MMM YYYY"
+//            val sdf = SimpleDateFormat(myFormat)
+//            val dateEDT = sdf.format(thisCountry.updated)
+//
+//            lastUpdtD.text = "Last Updated : $dateEDT"
+        }
+
+
+    }
+
+
+    private fun convertToIndianStandard(str: String) : String{
+        val len = str.length
+        return when {
+            len>5 -> {
+                var ans = str.substring(len-3)
+                for (i in len-3 downTo 1 step 2){
+                    ans = str.substring((i-2).coerceAtLeast(0),i)+","+ans
+                }
+                ans
+            }
+            len>3 -> {
+                str.substring(0, len-3)+","+str.substring(len-3)
+            }
+            else -> {
+                str
+            }
+        }
+    }
+
     fun openDetail(view: View) {
         if(view.nameView.text == "India"){
             startActivity(Intent(this,IndiaActivity::class.java))
         }
         else{
-            val i = Intent(this, DetailActivity::class.java)
-            i.putExtra("name", view.nameView.text.toString())
+            val i = Intent(this,DetailActivity::class.java)
+            i.putExtra("name",view.nameView.text)
             startActivity(i)
+            //openDialogDetail(view.nameView.text)
         }
     }
 
